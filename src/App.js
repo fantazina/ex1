49f043efbd0f2b20587dfb6220ff20211081aa68
fantazina <toryj666@gmail.com>
2025-01-18 02:06:54 +0900

    레이아웃 임시

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from "react";
+import Header from './components/Header';
+import Content from './components/Content';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+      <div>
+          <Header />
+          <Content />
+          <Footer />
+      </div>
+  );
+}
+
+export default App;
